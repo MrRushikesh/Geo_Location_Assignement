e8a19b19b354e94981da3a419e959197371ae2a0
MrRushikesh <rushikeshingale7447@gmail.com>
2022-12-03 14:43:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function loadCoupon(){
+    document.getElementById('coupon').style.visibility = 'visible';
+    document.getElementById('couponMain').style.opacity='0.5';
+}
+
+
+function closeCoupon(){
+    document.getElementById('coupon').style.visibility = 'hidden';
+    document.getElementById('couponMain').style.opacity='1';
+}
+
+
